372c13f356ee2af5cd108e3f5090cb93bee4b465
Kriba95 <kribadiba@gmail.com>
2021-05-01 18:50:13 +0300

    commit main

--- kukkuuhei.php
+++ kukkuuhei.php
@@ -17,34 +17,33 @@
     <hr>
       <div class="js-boksi">
         <h1 style="text-align: left;"><?php 
-//Vastaanottaa input arvon sivusta /index.php
-// $ Arkistoi arvon/tekstin
-$nimi = $_POST['nimi'];
+              //Vastaanottaa input arvon sivusta /index.php
+              // $ Arkistoi arvon/tekstin
+              $nimi = $_POST['nimi'];
 
-// Yhdistää databaseen 
-require('connection.php');
-$con = new mysqli($servername, $username, $password, $dbname);
+              // Yhdistää databaseen 
+              require('connection.php');
+              $con = new mysqli($servername, $username, $password, $dbname);
 
-// Jos yheteys epäonnistuu printtaa \n Yhteyttä
-if (mysqli_connect_errno()) {
-  printf("Yhteys epäonnistui, tarkista: \n", mysqli_connect_error());
-  exit();
-}
-// Taulut
-$query = "INSERT INTO `user`(`nimi`) VALUES ('$nimi')";
-$result = mysqli_query($con, $query);
+              // Jos yheteys epäonnistuu printtaa \n Yhteyttä
+              if (mysqli_connect_errno()) {
+                printf("Yhteys epäonnistui, tarkista: \n", mysqli_connect_error());
+                exit();
+              }
+              // Taulut
+              $query = "INSERT INTO `user`(`nimi`) VALUES ('$nimi')";
+              $result = mysqli_query($con, $query);
 
-// Jos yheteys epäonnistuu printtaa \n Yhteyttä
-if ($con->connect_error) {
-  die("Yhteys epäonnistui, tarkista: \n" . $con->connect_error);
-}
+              // Jos yheteys epäonnistuu printtaa \n Yhteyttä
+              if ($con->connect_error) {
+                die("Yhteys epäonnistui, tarkista: \n" . $con->connect_error);
+              }
 
-// Jos yheteys onnistuu printtaa teksti;
-else {
-  echo "Kiitos! Juttusi on lisätty :D";
-}
-$con->close(); ?>
-</h1>
+              // Jos yheteys onnistuu printtaa teksti;
+              else {
+                echo "Kiitos! Juttusi on lisätty :D";
+              }
+              $con->close(); ?></h1>
         <hr>
       </div>
     </div>
